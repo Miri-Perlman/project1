e86de792b377fe0475a01fad4be6b9b34c1a559b
Miri-Perlman <Hand1D29@mbjcomp.org.il>
2025-03-11 19:49:33 +0200

    miri change the project

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -25,6 +25,7 @@ const Child= (props) =>{
         <br />
         <Fab id="add" onClick={() => props.addChildFunc({ Name: Name, Age: Age ,City:City,IsAdult: IsAdult})}  size="medium" color="primary" aria-label="add">
         <AddIcon />
+        <h1>hi! my name is miri</h1>
       </Fab>
       </Box>   
       
